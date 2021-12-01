7c707144155347fba745664af2eed4a496abafa5
Nolan Trgovac <trgovac.nolan@gmail.com>
2021-11-30 22:42:04 -0500

    Static.Json build

--- src/Connect/API-URL.tsx
+++ src/Connect/API-URL.tsx
@@ -3,8 +3,8 @@ switch(window.location.hostname){
     case 'localhost' || '127.0.0.1':
     APIURL = 'http://localhost:3001';
     break;
- case'nct-Final.herokuapp.com':
- APIURL='https://nct.herokuapp.com'
+ case'nctfinal2345.herokuapp.com':
+ APIURL='https://nctfinal2345.herokuapp.com'
 break;
 }
 
