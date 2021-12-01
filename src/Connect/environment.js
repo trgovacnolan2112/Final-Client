31ae92afbc057303c889ad49b48bf5fa5e140106
Nolan Trgovac <trgovac.nolan@gmail.com>
2021-12-01 09:33:47 -0500

    Final

--- src/Connect/environment.js
+++ src/Connect/environment.js
@@ -1,7 +1,7 @@
 let APIURL= '';
 switch (window.location.hostname) {
     case 'localhost' || '127.0.0.1':
-    APIURL = 'https://localhost:3001';
+    APIURL = 'http://localhost:3001';
     break;
  case'nctfinal2345.herokuapp.com':
  APIURL='https://nctfinal2345.herokuapp.com'
